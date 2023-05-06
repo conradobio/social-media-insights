21c135b9073d6d91d368ae80207260642495a696
conradobio <conradobio@gmail.com>
2023-05-06 08:09:09 -0300

    feat - added s3 database

--- src/apify-api.py
+++ src/apify-api.py
@@ -5,9 +5,54 @@ import logging
 from datetime import datetime
 import json
 
-def main(client, params, date):
-    type = params.type
-    account = params.account
+import boto3
+import pandas as pd
+from datetime import datetime
+
+import threading
+import io
+import sys
+
+import os
+
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+APIFY_TOKEN = os.getenv('APIFY_TOKEN')
+AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION") 
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+raw_bucket = 'instagram-raw'
+analytics_bucket = 'intagram-analytics'
+
+accounts = ['ic.campinas', 'ic.saobernardo']
+types = ['details', 'posts']
+today = datetime.now().date()
+
+class UploadProgressPercentage(object):
+
+    def __init__(self, filename):
+        self._filename = filename
+        self._size = float(os.path.getsize(filename))
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
+
+    def __call__(self, bytes_amount):
+        # To simplify we'll assume this is hooked up
+        # to a single filename.
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            percentage = (self._seen_so_far / self._size) * 100
+            sys.stdout.write(
+                "\r%s  %s / %s  (%.2f%%) \n" % (
+                    self._filename, self._seen_so_far, self._size,
+                    percentage))
+            sys.stdout.flush()
+
+def pull_data_api(client, type, account, date):
+    # type = params.type
+    # account = params.account
 
     folder = account.split('.')[1]
     url = f"https://www.instagram.com/{account}/"
@@ -36,14 +81,39 @@ def main(client, params, date):
     items = []
     for item in client.dataset(run["defaultDatasetId"]).iterate_items():
         items.append(item)
+    
+    logging.info(f'Data Extracted - {len(items)}')
 
-    # save dictionary to json file
     file_path = f"./data/ic-{folder}/ic_{folder}-{type}-{date}.json"
     with open(file_path, 'w') as f:
         f.write(json.dumps(items))
     logging.info(f'Loaded data to {file_path}')
+    return file_path
+
+def load_s3(file_path, bucket_name, type, account, today):
+    # Starting a Session s3
+    logging.info(f'Connecting to S3')
+    session = boto3.Session(
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            region_name=AWS_DEFAULT_REGION
+        )
+
+    s3 = session.resource("s3")
+    bucket = s3.Bucket(name=bucket_name)
+    logging.info(f'Connected to {bucket_name}')
+
+    logging.info(f'Saving files to S3')
+    file_name = f'{account}/{account}-{type}-{today}.json'
+    with open(file_path, 'rb') as data:
+        bucket.upload_fileobj(data,
+            file_name,
+            Callback=UploadProgressPercentage(file_path)
+            )
+    logging.info(f'Saved file to S3')
 
 if __name__ == "__main__":
+
     logging.basicConfig(level=logging.INFO, 
                 filename="./logs/logs.log", 
                 format="%(asctime)s - %(levelname)s - %(message)s")
@@ -51,13 +121,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract Data from Instagram')
     parser.add_argument('--type', required=True, help='type - details / posts')
     parser.add_argument('--account', required=True, help='password for postgres')
-
     args = parser.parse_args()
 
-    date = datetime.now().strftime('%Y-%m-%d')
+    today = datetime.now().date()
 
     # Initialize the ApifyClient with your API token
-    apify_token = 'apify_api_6tcrmwgn0f7RQq98V7NL47VsS63Jpx4rA7BA'
+    apify_token = APIFY_TOKEN
     client = ApifyClient(apify_token)
 
-    main(client, args, date)
+    type = args.type
+    account = args.account
+
+    file_path = pull_data_api(client, type, account, today)
+
+    raw_bucket = 'instagram-raw'
+    load_s3(file_path, raw_bucket, type, account, today)
